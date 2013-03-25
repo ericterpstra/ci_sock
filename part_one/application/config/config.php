c72169e0080eadf5e31b1d268b98473d1500f13b
Eric Terpstra <eterps@gmail.com>
2013-03-24 23:29:30 -0500

    Working with Redis session instead of mysql

--- part_one/application/config/config.php
+++ part_one/application/config/config.php
@@ -358,5 +358,11 @@ $config['rewrite_short_tags'] = FALSE;
 $config['proxy_ips'] = '';
 
 
+$config['redis_host'] = 'localhost';
+$config['redis_port'] = '6379';
+
+
+
 /* End of file config.php */
 /* Location: ./application/config/config.php */
+
