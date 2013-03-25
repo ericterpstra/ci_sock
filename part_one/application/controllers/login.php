18bdddd7b9ecab4ffe198fd7c4f4618db25150b4
Eric Terpstra <eterps@gmail.com>
2013-03-24 21:20:40 -0500

    Cleaned up some comments

--- part_one/application/controllers/login.php
+++ part_one/application/controllers/login.php
@@ -13,15 +13,19 @@ class login extends CI_Controller {
     }
 
     function login_user() {
+        // Create an instance of the user model
         $this->load->model('user_m');
+
+        // Grab the email and password from the form POST
         $email = $this->input->post('email');
-
         $pass  = $this->input->post('password');
-        $login = false;
 
+        //Ensure values exist for email and pass, and validate the user's credentials
         if( $email && $pass && $this->user_m->validate_user($email,$pass)) {
+            // If the user is valid, redirect to the main view
             redirect('/main/show_main');
         } else {
+            // Otherwise show the login screen with an error message.
             $this->show_login(true);
         }
     }
--- part_one/application/models/user_m.php
+++ part_one/application/models/user_m.php
@@ -6,13 +6,19 @@ class user_m extends CI_Model {
     var $details;
 
     function validate_user( $email, $password ) {
+        // Build a query to retrieve the user's details
+        // based on the received username and password
         $this->db->from('user');
         $this->db->where('email',$email );
         $this->db->where( 'password', sha1($password) );
         $login = $this->db->get()->result();
 
+        // The results of the query are stored in $login.
+        // If a value exists, then the user account exists and is validated
         if ( is_array($login) && count($login) == 1 ) {
+            // Set the users details into the $details property of this class
             $this->details = $login[0];
+            // Call set_session to set the user's session vars via CodeIgniter
             $this->set_session();
             return true;
         }
@@ -21,6 +27,9 @@ class user_m extends CI_Model {
     }
 
     function set_session() {
+        // session->set_userdata is a CodeIgniter function that
+        // stores data in CodeIgniter's session storage.  Some of the values are built in
+        // to CodeIgniter, others are added.  See CodeIgniter's documentation for details.
         $this->session->set_userdata( array(
                 'id'=>$this->details->id,
                 'name'=> $this->details->firstName . ' ' . $this->details->lastName,
