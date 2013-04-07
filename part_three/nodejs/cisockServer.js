53f8cef58a3a20dddcf8c97464b961d1e490ed72
Eric Terpstra <eterps@gmail.com>
2013-04-07 11:42:46 -0500

    Added redis node package

--- part_three/nodejs/cisockServer.js
+++ part_three/nodejs/cisockServer.js
@@ -1,15 +1,25 @@
 
 var io = require('socket.io').listen(8080);
+var redis = require('redis');
 
 io.sockets.on('connection', function (socket) {
+  var rClient = redis.createClient();
 
   // Let everyone know it's working
   socket.emit('startup', { message: 'I Am Working!!' });
 
-
   socket.on('newPost', function (post,team) {
     //console.log(post);
     console.log('Broadcasting a post to team: ' + team.toString());
+
+    rClient.keys('*',function (err, keys) {
+      if (err) return console.log(err);
+
+      for(var i = 0, len = keys.length; i < len; i++) {
+          console.log(keys[i]);
+      }
+    });
+
     var broadcastData = {message: post, team: team};
     socket.broadcast.to(team.toString()).emit('broadcastNewPost',broadcastData);
     broadcastData.team = 'admin';
